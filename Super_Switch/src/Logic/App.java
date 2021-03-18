7dde9ea4e93a16119a2e518c8cbcfa4c8a108bd1
this_serra <riicardoserrano@gmail.com>
2021-03-18 13:01:59 -0400

    :rocket: Starting...
    ...

--- Super_Switch/src/Logic/App.java
+++ Super_Switch/src/Logic/App.java
@@ -1,8 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
+
 package Logic;
 
 /**
@@ -15,8 +13,11 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        // We'll hit this one with 20 
+        Control app = new Control(); 
+        
+//        new Interfaz(app).setVisible(true); 
+//        
+        app.start();
     }
     
 }
---
+++ Super_Switch/src/Logic/Control.java
@@ -0,0 +1,43 @@
+
+
+
+package Logic;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author this_serra
+ */
+public class Control {
+    
+    public void start() {
+        
+        
+        
+        JOptionPane.showConfirmDialog(null, "Este si lo reventamos ???", "Proyectazo", 0, JOptionPane.QUESTION_MESSAGE);
+        // El codigo abajo nos servirá para guiarnos de como usan las colas 
+        
+        
+//        /*Creamos la Cola Indicando el tipo de dato*/
+//        Queue<Integer> cola=new LinkedList();
+//        /*Insertamos datos*/
+//            cola.offer(3);
+//            cola.add(14);
+//            cola.offer(12);
+//            cola.add(7);
+//            cola.offer(10);
+//        /*Impresion de la Cola llena con los datos*/
+//        System.out.println("Cola llena: " + cola);
+//        /*Estructura repetitiva para desencolar*/
+//        while(cola.poll()!=null){//Desencolamos y el valor se compara con null
+//            System.out.println(cola.peek());//Muestra el nuevo Frente
+//        }
+//        /*Muestra null debido a que la cola ya esta vacia*/
+//        System.out.println(cola.peek());  
+        
+    }
+    
+}
