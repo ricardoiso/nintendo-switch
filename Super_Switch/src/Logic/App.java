da127430b3d1915bb2841c1dfe8330bafe64cb36
this_serra <riicardoserrano@gmail.com>
2021-03-18 12:25:24 -0400

    :rocket: First stuff
    Here we go again.

---
+++ Super_Switch/src/Logic/App.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+/**
+ *
+ * @author this_serra
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // We'll hit this one with 20 
+    }
+    
+}
